24dabd78cf2214c019043290a3cc485808c04330
krishnakalyana <krishnakalyanax@gmail.com>
2020-07-01 22:26:24 +0530

    Initial Commit

---
+++ 12v/src/ClassComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function ClassComponent(){
+    
+        return(
+            <div className='CClass'>
+            <div>Name : John</div>
+            <div>Age : 36</div>
+            </div>
+        )
+    
+}
+export default ClassComponent
